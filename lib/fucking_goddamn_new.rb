e0cf175307c925632a2b2f463740b5c42332d9a1
Brandon Mason <brandonjmason@gmail.com>
2011-12-04 00:01:39 -0700

    added optional args, added [goddammit, cunt, ass, shit]

--- lib/fucking_goddamn_new.rb
+++ lib/fucking_goddamn_new.rb
@@ -1,11 +1,11 @@
 class Object
 
-  %w( fucking fuck damn god damn goddamn bitch chickenfucker motherfucker ).each do |word|
+  %w( fucking fuck damn god goddamn goddammit cunt ass shit bitch chickenfucker motherfucker ).each do |word|
     sym = word.to_sym
-    define_method(sym) { self } unless respond_to?(sym)
+    define_method(sym) {|*args| self } unless respond_to?(sym)
 
     sym = "#{ word }!".to_sym
-    define_method(sym) { self } unless respond_to?(sym)
+    define_method(sym) {|*args| self } unless respond_to?(sym)
   end
 
 end
